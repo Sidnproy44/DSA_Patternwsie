54ee67d1f06c909d264923e1ec43e3d8d9bb3c8d
Siddheshwar <heysid.proy@gmail.com>
2026-03-26 13:48:10 +0530

    twoSum-2.py

---
+++ 2PointerApproach/twoSum-2.py
@@ -0,0 +1,27 @@
+# Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+
+# Return the indices of the two numbers index1 and index2, each incremented by one, as an integer array [index1, index2] of length 2.
+
+# The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+# Your solution must use only constant extra space.
+
+class solution():
+    def twoSum(this, numbers, target):
+        left , right = 0 , len(numbers) -1
+
+        while left < right :
+            if numbers[left] + numbers[right] == target:
+                return [left, right]
+            elif numbers[left] + numbers[right] < target:
+                left+=1
+            elif numbers[left] + numbers[right] > target:
+                right-=1
+
+        return None
+
+testSubject1 = solution()
+test2 = solution()
+
+print(testSubject1.twoSum([1, 2 ,3, 4, 5], 6))                
+print(test2.twoSum([2, 7, 11, 15], 9))                
